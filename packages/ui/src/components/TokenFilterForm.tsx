9547e346284b459a23e3f2a573ea7196b97502f8
W.J. “dysbulic” H <dys@dhappy.org>
2022-08-23 13:10:41 -0400

    restructurin' form & fixin' visible list's display ₩

--- packages/ui/src/components/TokenFilterForm.tsx
+++ packages/ui/src/components/TokenFilterForm.tsx
@@ -1,6 +1,9 @@
 import { toSpanList } from '@/lib/helpers'
 import { Limits } from '@/lib/types'
-import { Box, Button, Checkbox, Flex, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import {
+  chakra, Button, Checkbox, Flex, FormControl,
+  FormLabel, Input, Stack, type FlexProps, Text,
+} from '@chakra-ui/react'
 import React, { SetStateAction, useEffect, useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 
@@ -20,89 +23,107 @@ export const TokenFilterForm: React.FC<{
   setGatingVisible: (gatingVisible: SetStateAction<boolean>) => void
   setVisibleList: (visible: SetStateAction<Array<number | Limits>>) => (
     void
-  ) 
-}> = ({
+  )
+  visibleList: Array<number | Limits> 
+} & FlexProps> = ({
   limit = 10, setLimit, offset = 0, setOffset,
   gatingVisible = false, setGatingVisible,
-  setVisibleList
+  visibleList, setVisibleList, ...props
 }) => {
-  const [visible, setVisible] = useState(null)
   const {
     register, handleSubmit, control, setValue,
   } = useForm<FilterValues>()
-  const submit = async (data: FilterValues) => {
-    setLimit(Number(data.limit))
-    setOffset(Number(data.offset))
-    setGatingVisible(data.gatingVisible)
-    setVisible(data.visible)
-    setVisibleList(toSpanList(data.visible))
-  }
 
   useEffect(() => {
     setValue('limit', limit)
     setValue('offset', offset)
-    setValue('visible', visible)
+    setValue('visible', visibleList.toString())
     setValue('gatingVisible', gatingVisible)
-  }, [limit, offset, visible, gatingVisible, setValue])
+  }, [limit, offset, visibleList, gatingVisible, setValue])
+
+  const submit = async (data: FilterValues) => {
+    setLimit(Number(data.limit))
+    setOffset(Number(data.offset))
+    setGatingVisible(data.gatingVisible)
+    setVisibleList(toSpanList(data.visible))
+  }
 
   return (
-    <Box
+    <Flex
       as="form" onSubmit={handleSubmit(submit)}
       mt={10} mb="1rem" maxW={['100%', 'min(85vw, 50em)']}
+      direction={['column', 'row']}
       sx={{ a: { textDecoration: 'underline' } }}
+      {...props}
     >
-      <FormControl>
-        <Flex align="center" my={1}>
-          <FormLabel _after={{ content: '":"' }}>Limit</FormLabel>
-          <Input
-            type="number"
-            placeholder="Number of tokens to display."
-            {...register('limit')}
-          />
-        </Flex>
-      </FormControl>
-      <FormControl>
-        <Flex align="center" my={1}>
-          <FormLabel _after={{ content: '":"' }}>Offset</FormLabel>
-          <Input
-            type="number"
-            placeholder="Number of tokens offset from Token 1."
-            {...register('offset')}
-          />
-        </Flex>
-      </FormControl>
-      <FormControl>
-        <Flex align="center" my={1} >
-          <FormLabel _after={{ content: '":"' }}>Visible&#xA0;List</FormLabel>
-          <Input
-            placeholder="Comma, space and dash separated list of indices."
-            {...register('visible')}
-          />
-        </Flex>
-      </FormControl>
-      <FormControl>
-        <Flex align="center" my={1}>
-          <Controller
-            control={control}
-            name="gatingVisible"
-            defaultValue={gatingVisible}
-            render={({ field: { onChange, value, ref } }) => (
-              <Checkbox
-                onChange={onChange}
-                ref={ref}
-                isChecked={value}
-              >
-                View&#xA0;Permission&#xA0;Tokens
-              </Checkbox>
-            )}
-          />
-        </Flex>
-      </FormControl>
+      <Stack
+        flexGrow={1}
+        sx={{
+          '&>*:not(style)~*:not(style)': { mt: 0.5 },
+          label: {
+            _after: { content: '":"' },
+            mt: 1.5,
+            mr: 1,
+            fontSize: '110%',
+          },
+        }}
+      >
+        <FormControl>
+          <Flex align="center">
+            <FormLabel>Limit</FormLabel>
+            <Input
+              type="number"
+              placeholder="Number of tokens to display."
+              {...register('limit')}
+            />
+          </Flex>
+        </FormControl>
+        <FormControl>
+          <Flex align="center">
+            <FormLabel>Offset</FormLabel>
+            <Input
+              type="number"
+              placeholder="Number of tokens offset from Token 1."
+              {...register('offset')}
+            />
+          </Flex>
+        </FormControl>
+        <Text textAlign="center">or</Text>
+        <FormControl>
+          <Flex align="center">
+            <FormLabel>Visible&#xA0;List</FormLabel>
+            <Input
+              placeholder="Comma, space and dash separated list of indices."
+              {...register('visible')}
+            />
+          </Flex>
+        </FormControl>
+      </Stack>
+      <Stack ml={3}>
+        <FormControl>
+          <Flex align="center" my={1}>
+            <Controller
+              control={control}
+              name="gatingVisible"
+              defaultValue={gatingVisible}
+              render={({ field: { onChange, value, ref } }) => (
+                <Checkbox
+                  onChange={onChange}
+                  ref={ref}
+                  isChecked={value}
+                >
+                  View<chakra.br/>Permission<chakra.br/>Tokens
+                </Checkbox>
+              )}
+            />
+          </Flex>
+        </FormControl>
 
-      <Button type="submit">
-        View
-      </Button>
-    </Box>
+        <Button type="submit" colorScheme="purple">
+          View
+        </Button>
+      </Stack>
+    </Flex>
   )
 }
 
