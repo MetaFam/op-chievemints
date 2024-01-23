ab29477e8e18c5ed823cd9991e2ae36bdc384d40
dysbulic <dys@dhappy.org>
2024-01-22 20:58:53 -0500

    Updates

---
+++ assets/HomeLink-80873577.js
@@ -0,0 +1,14 @@
+import { j as jsx } from "./index-ded11d65.js";
+import { L as LinkedSVG } from "./TokenFilterForm.module-d3dfb62e.js";
+const HomeLink = () => /* @__PURE__ */ jsx(
+  LinkedSVG,
+  {
+    href: "/",
+    svg: "/favicon.svg",
+    id: "homeLink"
+  }
+);
+export {
+  HomeLink as H
+};
+//# sourceMappingURL=HomeLink-80873577.js.map
